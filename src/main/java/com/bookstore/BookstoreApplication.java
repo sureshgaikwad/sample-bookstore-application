8dd22d11295246744f3edfc6e9d211702be88c4e
sureshgaikwad <sureshgaikwad309@gmail.com>
2025-09-15 15:56:26 +0530

    Update to v1.0.2 - test GitOps fix without chmod

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Bookstore Application - A simple REST API for managing books with rating feature
  * 
  * @author Suresh Gaikwad
- * @version 1.0.1
+ * @version 1.0.2
+ * Build Date: 2025-09-15
  */
 @SpringBootApplication
 public class BookstoreApplication {
